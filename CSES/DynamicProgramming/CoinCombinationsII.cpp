62d5941e6c0772dd9f933234ab4d6ad53f438bd3
Orsted <cuong.vm.21@gmail.com>
2025-04-12 16:23:18 +0700

    final solution for Coin Combinations II (still dont get it)

--- CSES/DynamicProgramming/CoinCombinationsII.cpp
+++ CSES/DynamicProgramming/CoinCombinationsII.cpp
@@ -7,6 +7,7 @@
 #include <climits>
 #include <ctime>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 #define ll long long
@@ -28,30 +29,12 @@ int recursive(int *c, int x, int size, int pos = 0) {
 	return ans;
 }
 
-int dp_top_down[mxN + 1];
-int top_down_recursive(int *c, int x, int size, int pos = 0) {
-	if (x < 0)
-		return 0;
-
-	if (dp_top_down[x] != -1)
-		return dp_top_down[x];
-
-	int ans = 0;
-	for (int i = pos; i < size; ++i) {
-		ans = (ans + top_down_recursive(c, x - c[i], size, i)) % MOD;
-	}
-
-	dp_top_down[x] = ans;
-	return dp_top_down[x];
-}
-
 int bottom_up(int *c, int x, int size) {
-	int dp[x + 1];
+	vector<int> dp(x + 1, 0);
 	dp[0] = 1;
 
-	for (int i = 1; i <= x; ++i) {
-		dp[i] = 0;
-		for (int j = 0; j < size; ++j) {
+	for (int j = 0; j < size; ++j) {
+		for (int i = 1; i <= x; ++i) {
 			if (i - c[j] >= 0) {
 				dp[i] = (dp[i] + dp[i - c[j]]) % MOD;
 			}
@@ -68,20 +51,11 @@ int main(int argc, char **argv) {
 	for (int i = 0; i < n; ++i)
 		cin >> c[i];
 
-	sort(c, c + n);
-
 	// clock_t tStart = clock();
-	cout << recursive(c, x, n) << endl;
+	// cout << recursive(c, x, n) << endl;
 	// printf("%fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 
-	// tStart = clock();
-	for (int i = 0; i <= x; ++i)
-		dp_top_down[i] = -1;
-	dp_top_down[0] = 1;
-	cout << top_down_recursive(c, x, n) << endl;
-	// printf("%fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
-	//
-	// cout << bottom_up(c, x, n);
+	cout << bottom_up(c, x, n);
 
 	return 0;
 }
