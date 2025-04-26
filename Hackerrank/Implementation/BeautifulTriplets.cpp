145b3af657af5587fac0cda9f7df6d4a7c0b6b43
Orsted <cuong.vm.21@gmail.com>
2025-04-26 15:10:31 +0700

    the optimized solution for Beautiful Triplets problem in Hackerrank (O(n))

--- Hackerrank/Implementation/BeautifulTriplets.cpp
+++ Hackerrank/Implementation/BeautifulTriplets.cpp
@@ -10,24 +10,32 @@ using namespace std;
 
 const int mxN = 1e4;
 int n, d;
-int a[mxN];
+int a[mxN], h[2 * mxN + 1]{};
 
 int main(int argc, char **argv) {
 	cin >> n >> d;
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; ++i) {
 		cin >> a[i];
-
-	int ans = 0;
-	for (int i = 0; i < n - 2; ++i) {
-		for (int j = i + 1; j < n - 1; ++j) {
-			if (a[j] - a[i] == d)
-				for (int k = j + 1; k < n; ++k) {
-					if (a[k] - a[j] == d)
-						++ans;
-				}
-		}
+		++h[a[i]];
 	}
 
+	// int ans = 0;
+	// for (int i = 0; i < n - 2; ++i) {
+	// 	for (int j = i + 1; j < n - 1; ++j) {
+	// 		if (a[j] - a[i] == d)
+	// 			for (int k = j + 1; k < n; ++k) {
+	// 				if (a[k] - a[j] == d)
+	// 					++ans;
+	// 			}
+	// 	}
+	// }
+
+	int ans = 0;
+	for (int i = 0; i < n; ++i) {
+		if (a[i] + d <= 2e4 && a[i] + 2 * d <= 2e4)
+			if (h[a[i] + d] != 0 && h[a[i] + 2 * d] != 0)
+				ans++;
+	}
 	cout << ans;
 
 	return 0;
