b347bf6837bd511de59b0f24bc6964b0cfbb8aab
Orsted <cuong.vm.21@gmail.com>
2025-05-02 20:11:51 +0700

    my solution for Absolute Permutation in hackerrank

---
+++ Hackerrank/Implementation/AbsolutePermutation.cpp
@@ -0,0 +1,46 @@
+/***********************************************
+ * File:        AbsolutePermutation.cpp
+ * Date:        2025 May 02 15:45:38
+ * Description: Basic C++ program template
+ ***********************************************/
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+#define ll long long
+
+const int mxN = 1e5 + 1;
+int t, n, k;
+int a[mxN];
+
+int main(int argc, char **argv) {
+	cin >> t;
+
+	while (t--) {
+		cin >> n >> k;
+		for (int i = 1; i <= n; ++i)
+			a[i] = i;
+
+		bool flag = true;
+		do {
+			for (int i = 1; i <= n; ++i) {
+				if (abs(a[i] - i) != k) {
+					flag = false;
+					break;
+				}
+				flag = true;
+			}
+			if (flag)
+				break;
+		} while (next_permutation(a + 1, a + n + 1));
+		if (!flag)
+			cout << "-1";
+		else
+			for (int i = 1; i <= n; ++i)
+				cout << a[i] << " ";
+		cout << endl;
+	}
+
+	return 0;
+}
