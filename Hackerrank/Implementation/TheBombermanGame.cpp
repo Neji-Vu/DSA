c7118cd7bc08bbd73a0f763ce49af0f3a17fe1af
Orsted <cuong.vm.21@gmail.com>
2025-05-03 16:18:48 +0700

    my solution for The Bomberman Game problem in Hackerrank

---
+++ Hackerrank/Implementation/TheBombermanGame.cpp
@@ -0,0 +1,62 @@
+/***********************************************
+ * File:        TheBombermanGame.cpp
+ * Date:        2025 May 03 12:43:31
+ * Description: Basic C++ program template
+ ***********************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+#define ll long long
+
+int n, r, c;
+vector<string> g;
+int dx[4]{-1, 1, 0, 0}, dy[4]{0, 0, -1, 1};
+
+bool e(int row, int col) {
+	return row >= 0 && row < r && col >= 0 && col < c;
+}
+
+int main(int argc, char **argv) {
+	cin >> r >> c >> n;
+	for (int i = 0; i < r; ++i) {
+		string temp;
+		cin >> temp;
+		g.push_back(temp);
+	}
+
+	if (n == 1) {
+		for (string s : g)
+			cout << s << endl;
+		return 0;
+	}
+
+	if (n % 2 == 0) {
+		for (int i = 0; i < r; ++i)
+			cout << string(c, 'O') << endl;
+		return 0;
+	}
+
+	for (int i = 3; i <= n; i += 2) {
+		vector<string> temp(r, string(c, 'O'));
+		for (int row = 0; row < r; ++row) {
+			for (int col = 0; col < c; ++col) {
+				if (g[row][col] == 'O') {
+					temp[row][col] = '.';
+					for (int k = 0; k < 4; ++k) {
+						if (e(row + dx[k], col + dy[k])) {
+							temp[row + dx[k]][col + dy[k]] = '.';
+						}
+					}
+				}
+			}
+		}
+		g = temp;
+	}
+
+	for (string s : g)
+		cout << s << endl;
+
+	return 0;
+}
