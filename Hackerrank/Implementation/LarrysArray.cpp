8b410f10a693bb8d032d0ff0b5a22c371a3b6583
Orsted <cuong.vm.21@gmail.com>
2025-05-05 20:14:28 +0700

    the optimized solution for Larry's Array problem in Hackerrank

--- Hackerrank/Implementation/LarrysArray.cpp
+++ Hackerrank/Implementation/LarrysArray.cpp
@@ -20,47 +20,44 @@ int main(int argc, char **argv) {
 		for (int i = 1; i <= n; ++i)
 			cin >> a[i];
 
-		for (int i = 1; i <= n - 2; ++i) {
-			if (a[i] != i) {
-				for (int j = i + 1; j <= n; ++j) {
-					if (a[j] == i) {
-						if (j - i == 1) {
-							int temp = a[i];
-							a[i] = a[i + 1];
-							a[i + 1] = a[i + 2];
-							a[i + 2] = temp;
-							break;
-						}
-						if ((j - i) % 2 == 0) {
-							for (int k = j; k >= i + 1; --k)
-								a[k] = a[k - 1];
-						} else {
-							for (int k = j; k >= i + 2; --k)
-								a[k] = a[k - 1];
-							a[i + 2] = a[i];
-						}
-						a[i] = i;
-						break;
-					}
-				}
-			}
-		}
-
-		// if (a[n - 2] != n - 2) {
-		// 	if (a[n - 1] == n - 2) {
-		// 		int temp = a[n - 2];
-		// 		a[n - 2] = a[n - 1];
-		// 		a[n - 1] = a[n];
-		// 		a[n] = temp;
-		// 	} else {
-		// 		int temp = a[n];
-		// 		a[n] = a[n - 1];
-		// 		a[n - 1] = a[n - 2];
-		// 		a[n] = temp;
+		// for (int i = 1; i <= n - 2; ++i) {
+		// 	if (a[i] != i) {
+		// 		for (int j = i + 1; j <= n; ++j) {
+		// 			if (a[j] == i) {
+		// 				if (j - i == 1) {
+		// 					int temp = a[i];
+		// 					a[i] = a[i + 1];
+		// 					a[i + 1] = a[i + 2];
+		// 					a[i + 2] = temp;
+		// 					break;
+		// 				}
+		// 				if ((j - i) % 2 == 0) {
+		// 					for (int k = j; k >= i + 1; --k)
+		// 						a[k] = a[k - 1];
+		// 				} else {
+		// 					for (int k = j; k >= i + 2; --k)
+		// 						a[k] = a[k - 1];
+		// 					a[i + 2] = a[i];
+		// 				}
+		// 				a[i] = i;
+		// 				break;
+		// 			}
+		// 		}
 		// 	}
 		// }
+		//
+		// if (a[n] - a[n - 1] == 1)
+		// 	cout << "YES\n";
+		// else
+		// 	cout << "NO\n";
 
-		if (a[n] - a[n - 1] == 1)
+		int cnt = 0;
+		for (int i = 1; i <= n; ++i)
+			for (int j = i + 1; j <= n; ++j)
+				if (a[i] > a[j])
+					++cnt;
+
+		if (cnt % 2 == 0)
 			cout << "YES\n";
 		else
 			cout << "NO\n";
