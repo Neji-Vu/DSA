6ca724bcd6aa401d6ebdad904662f1ae0d056ab7
Orsted <cuong.vm.21@gmail.com>
2025-05-06 02:37:26 +0700

    creating insertion sort

--- ListOfAlgorithms/Sorting/Sorting.cpp
+++ ListOfAlgorithms/Sorting/Sorting.cpp
@@ -16,12 +16,14 @@ void print(array<int, 10> arr) {
 	for (int i = 0; i < 10; ++i) {
 		cout << arr[i] << " ";
 	}
-	cout << endl;
+
+	cout << endl << endl;
 }
 
 array<int, 10> BubbleSort() {
 	array<int, 10> ret = a;
 
+	cout << "Bubble Sort:\n";
 	for (int i = ret.size() - 1; i >= 0; --i)
 		for (int j = 0; j < i; ++j)
 			if (ret[j] > ret[j + 1])
@@ -30,10 +32,30 @@ array<int, 10> BubbleSort() {
 	return ret;
 }
 
+array<int, 10> InsertionSort() {
+	array<int, 10> ret = a;
+
+	cout << "Insertion Sort:\n";
+	for (int i = 1; i < ret.size(); ++i) {
+		int x = ret[i];
+		int pos = i;
+
+		while (pos >= 0 && x < ret[pos - 1]) {
+			ret[pos] = ret[pos - 1];
+			--pos;
+		}
+
+		ret[pos] = x;
+	}
+
+	return ret;
+}
+
 int main(int argc, char **argv) {
 	print(a);
 
 	print(BubbleSort());
+	print(InsertionSort());
 
 	return 0;
 }
